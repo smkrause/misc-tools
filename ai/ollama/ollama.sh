965149fb08647c65c90dcdddb1a9dc031982b24f
Steve K <smkrause@users.noreply.github.com>
2025-01-14 10:01:21 -0800

    Update ollama.sh
    update llama model

--- ai/ollama/ollama.sh
+++ ai/ollama/ollama.sh
@@ -2,4 +2,4 @@
 docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
 # Pull the latest Ollama 3.1 model
-docker exec -it ollama ollama pull llama3.1
+docker exec -it ollama ollama pull llama3.3
